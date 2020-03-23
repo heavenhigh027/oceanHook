495f7ae317769016519fec8f12849a58a279324d
heavenhigh027 <heavenhigh027@gmail.com>
2020-03-23 17:07:10 +0700

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,18 @@
+const secret = "your_secret_here";
+const repo = "~/your_repo_path_here/";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            exec('cd ' + repo + ' && git pull');
+        }
+    });
+
+    res.end();
+}).listen(8080);
